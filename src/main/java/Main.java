20d3db791ae6f760b44109e9a72961926db3a5fd
EvVlF <ev.vl.fedorov@gmail.com>
2022-07-26 03:02:24 +0400

    Instruction unclear

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * This is a Javadoc for exercise
+     */
+
+    @Deprecated
+    public static void main(String[] args) {
+        String text = "It's not clear";
+        org.apache.commons.lang3.text.WordUtils.capitalize(text);
+    }
+}
